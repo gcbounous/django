7183668ff045a5f04c1fa163276e1416b98a8529
gcbounous <gccbounous@gmail.com>
2019-04-30 16:47:27 +0200

    middleware

--- crepes/crepes/settings.py
+++ crepes/crepes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'stats',
 )
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'stats.middleware.stats_middleware',
 ]
 
 ROOT_URLCONF = 'crepes.urls'
